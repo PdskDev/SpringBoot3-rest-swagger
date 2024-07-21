965b290b472dc0071c9faf51c0d830bb7b48aaf0
NadetDev <nadetdev@gmail.com>
2024-07-21 21:04:31 +0200

    Refactoring PlayerService

--- src/main/java/me/nadetdev/rest_football/services/PlayerService.java
+++ src/main/java/me/nadetdev/rest_football/services/PlayerService.java
@@ -6,6 +6,7 @@ import me.nadetdev.rest_football.model.Player;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,13 +14,11 @@ import java.util.stream.Collectors;
 @Service
 public class PlayerService {
 
-    private final Map<String, Player> players = Map.ofEntries(
-            Map.entry("1", new Player("1", 1, "Messi", "Forward", LocalDate.of(1986, 1, 10))),
-            Map.entry("2", new Player("2", 2, "Ronaldo", "Forward", LocalDate.of(1995, 3, 5))),
-            Map.entry("3", new Player("3", 3, "Neymar", "Forward", LocalDate.of(2000, 7, 20))),
-            Map.entry("4", new Player("4", 4, "Mbappe", "Forward", LocalDate.of(2001, 12, 3))),
-            Map.entry("6", new Player("6", 6, "Pogba", "Forward", LocalDate.of(1998, 5, 1)))
-    );
+    private Map<String, Player> players = new HashMap<>();
+
+    public PlayerService(Map<String, Player> players) {
+        generateDummyPlayersList(players);
+    }
 
     public List<Player> listPlayers(){
         return players.values().stream().collect(Collectors.toList());
@@ -56,5 +55,23 @@ public class PlayerService {
         if (players.containsKey(id)) {
             players.remove(id);
         }
+    }
+
+    private Map<String, Player> generateDummyPlayersList(Map<String, Player> listOfPlayers){
+        Player messi = new Player("1", 1, "Messi", "Forward", LocalDate.of(1986, 1, 10));
+        Player ronaldo = new Player("2", 2, "Ronaldo", "Forward", LocalDate.of(1995, 3, 5));
+        Player neymar = new Player("3", 3, "Neymar", "Forward", LocalDate.of(2000, 7, 20));
+        Player mbappe = new Player("4", 4, "Mbappe", "Forward", LocalDate.of(2001, 12, 3));
+        Player kante = new Player("5", 5, "Kante", "Forward", LocalDate.of(2011, 10, 8));
+        Player pogba = new Player("6", 6, "Pogba", "Forward", LocalDate.of(1998, 5, 1));
+
+        players.put(messi.id(), messi);
+        players.put(ronaldo.id(), ronaldo);
+        players.put(neymar.id(), neymar);
+        players.put(mbappe.id(), mbappe);
+        players.put(kante.id(), kante);
+        players.put(pogba.id(), pogba);
+
+        return players;
     }
 }
